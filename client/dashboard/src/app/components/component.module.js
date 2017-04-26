3197eb2bf31469eb162fdf52c350e1e12ab49284
Ayush Bahuguna <lespaulayush@gmail.com>
2017-04-26 23:27:05 +0530

    Add user module

--- client/dashboard/src/app/components/component.module.js
+++ client/dashboard/src/app/components/component.module.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import {post} from './post/post.module';
-import {login} from './login/login.module.js'
+import {login} from './login/login.module.js';
+import {user} from './user/user.module.js';
 
-export const components = angular.module('components', [post, login]).name
+export const components = angular.module('components', [post, login, user]).name
