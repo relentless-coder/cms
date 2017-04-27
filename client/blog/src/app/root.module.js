f161d0d83ab8b1aed7c3b8e0aed8ebf7daafdfcd
Ayush Bahuguna <lespaulayush@gmail.com>
2017-04-27 08:22:34 +0530

    Add new module dependencies

--- client/blog/src/app/root.module.js
+++ client/blog/src/app/root.module.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import {header} from './common/header/header.module.js'
 import {rootComponentName, rootComponent} from './root.component';
 
-angular.module('blog', [uiRouter])
+angular.module('blog', [uiRouter, header])
 .component(rootComponentName, rootComponent)
 .config(['$stateProvider', '$locationProvider', ($stateProvider, $locationProvider)=>{
   $locationProvider.html5Mode(true);
