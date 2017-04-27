bd50d31e2b109a0c5c061276b942b7515e5379e3
Ayush Bahuguna <lespaulayush@gmail.com>
2017-04-27 08:23:43 +0530

    apply changes to user routes

--- client/dashboard/src/app/components/user/user.factory.js
+++ client/dashboard/src/app/components/user/user.factory.js
@@ -2,13 +2,13 @@ export const userFactory = 'userFactory';
 export const userFactoryFunc = function($http, $localStorage){
   return {
     getUser: function(){
-      return $http.get('/user');
+      return $http.get('/admin/user');
     },
     storeUser: function(value){
       $localStorage.user = value;
     },
     editUser: function editUser(value) {
-      return $http.put('/user', value)
+      return $http.put('/admin/user', value)
     }
   }
 }
