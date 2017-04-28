ca9f0988ec9f0be53fd97e89f17678c286b86ccf
Ayush Bahuguna <lespaulayush@gmail.com>
2017-04-28 23:31:01 +0530

    add components dependency

--- client/blog/src/app/root.module.js
+++ client/blog/src/app/root.module.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import {header} from './common/header/header.module.js'
+import {header} from './common/header/header.module.js';
+import {navbar} from './common/navbar/navbar.module.js';
+import {components} from './components/components.module.js'
 import {rootComponentName, rootComponent} from './root.component';
+import './root.scss';
 
-angular.module('blog', [uiRouter, header])
+angular.module('blog', [uiRouter, header, navbar, components])
 .component(rootComponentName, rootComponent)
 .config(['$stateProvider', '$locationProvider', ($stateProvider, $locationProvider)=>{
   $locationProvider.html5Mode(true);
