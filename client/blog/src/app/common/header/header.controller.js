1d3b672c39cf56b60c48844ceddc59db3c9ea22a
Ayush Bahuguna <lespaulayush@gmail.com>
2017-04-28 23:28:27 +0530

    header component

--- client/blog/src/app/common/header/header.controller.js
+++ client/blog/src/app/common/header/header.controller.js
@@ -1,11 +1,16 @@
-function headerController(userFactory){
+function headerController(userFactory, $localStorage){
   const ctrl = this;
   ctrl.$onInit = function(){
-    userFactory.getUserInfo().then((data)=>{
-      console.log(data);
-      ctrl.user = data.data.user;
-    })
-  }
+    if($localStorage.user){
+      ctrl.user = $localStorage.user;
+    } else {
+       userFactory.getUserInfo().then((data)=>{
+        console.log(data);
+        ctrl.user = data.data.user;
+        $localStorage.user = data.data.user;
+      });
+    }
+  };
 }
 
 export {headerController}
--- client/blog/src/app/common/header/header.module.js
+++ client/blog/src/app/common/header/header.module.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
 import ngSanitize from 'angular-sanitize';
+import 'ngstorage';
 import {userFactory, userFactoryFunc} from './user.factory';
 import {headerComponentName, headerComponentOptions} from './header.component.js';
+import './header.scss';
 
-export const header = angular.module('header', [ngSanitize])
+export const header = angular.module('header', [ngSanitize, 'ngStorage'])
 .component(headerComponentName, headerComponentOptions)
 .factory(userFactory, ['$http', userFactoryFunc])
 .name
