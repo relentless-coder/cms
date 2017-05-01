eb0698507c97968368b79eabd1e2b3e82c7112f2
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-01 23:24:29 +0530

    add new module dependencies

--- client/blog/src/app/components/components.module.js
+++ client/blog/src/app/components/components.module.js
@@ -1,6 +1,7 @@
 import angular from 'angular'
 import {home} from './home/home.module'
 import {contact} from './contact/contact.module'
+import {post} from './post/post.module'
 
-export const components = angular.module('components', [home, contact])
+export const components = angular.module('components', [home, contact, post])
 .name
