2fe44f8f1b92c0935f23b0e75c96ed0986937c71
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-01 23:19:59 +0530

    post component module

---
+++ client/blog/src/app/components/post/post.module.js
@@ -0,0 +1,7 @@
+import angular from 'angular'
+import {posts} from './posts/posts.module.js'
+import {post_single} from './post_single/post.single.module'
+import {comment} from './comment/comment.module'
+
+export const post = angular.module('post', [posts, post_single, comment])
+.name
