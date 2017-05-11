b0bb7527cdf51ff483c41c06ae01ab1a8f74d8f6
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-11 08:40:47 +0530

    import single post styles

--- client/blog/src/app/components/post/post_single/post.single.module.js
+++ client/blog/src/app/components/post/post_single/post.single.module.js
@@ -3,6 +3,7 @@ import uiRouter from 'angular-ui-router'
 import ngSanitize from 'angular-sanitize'
 import {postSingleFactory, postSingleFactoryFunc} from './post.single.factory'
 import {postSingleComponent, postSingleComponentOptions} from './post.single.component'
+import './post.single.scss'
 
 export const post_single = angular.module('postSingle', [uiRouter, ngSanitize])
 .config(['$stateProvider', ($stateProvider)=>{
