f5879e5b24481122d8589a4876dbb60d7949b3cb
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-11 08:41:59 +0530

    import home styles

--- client/blog/src/app/components/home/home.module.js
+++ client/blog/src/app/components/home/home.module.js
@@ -3,6 +3,7 @@ import uiRouter from 'angular-ui-router'
 import 'ngstorage'
 import ngSanitize from 'angular-sanitize';
 import {homeComponent, homeComponentOptions} from './home.component'
+import './home.scss'
 
 export const home = angular.module('home', ['ngStorage', ngSanitize, uiRouter])
 .config(['$stateProvider', ($stateProvider)=>{
