87035e4ea1cedf9dc8010ecbe04d186893f57107
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-22 23:18:24 +0530

    import sidebar scss file

--- client/dashboard/src/app/common/sidebar.module.js
+++ client/dashboard/src/app/common/sidebar.module.js
@@ -2,10 +2,13 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import 'ngstorage';
 import {tokenFactory, tokenFactoryFunc} from '../components/login/token.factory.js'
+import {notifsFactory, notifsFactoryFunc} from './notifs.factory'
 import {sidebarComponentName, sidebarComponent} from './sidebar.component';
 import {sidebarController} from './sidebar.controller';
+import './sidebar.scss';
 
 export const sidebar = angular.module('sidebar', [uiRouter, 'ngStorage'])
 .component(sidebarComponentName, sidebarComponent)
 .factory(tokenFactory, ['$localStorage', tokenFactoryFunc])
+.factory(notifsFactory, ['$http', notifsFactoryFunc])
 .name;
