f76b0afbb3029fee8bcf38d180bc7ca5bc792a44
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-22 08:33:05 +0530

    remove showHeader directive

--- client/blog/src/app/common/navbar/navbar.module.js
+++ client/blog/src/app/common/navbar/navbar.module.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import 'ngstorage';
 import {navComponent, navComponentOptions} from './navbar.component';
+import {showHeader} from './showheader.directive'
 import './navbar.scss';
 
 export const navbar = angular.module('navbar', ['ngStorage'])
 .component(navComponent, navComponentOptions)
+.directive('showHeader', showHeader)
 .name;
