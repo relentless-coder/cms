7016c0726d7c8dfbba359a046cfc6b5d0c91e709
Ayush Bahuguna <lespaulayush@gmail.com>
2017-05-28 23:12:47 +0530

    add get all comments route

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -9,16 +9,28 @@ const sanitizer = require('sanitize-html');
 const app = express();
 const router = express.Router();
 
+router.get('/admin/comment', (req, res)=>{
+	Comment.find({}).populate('post', 'title').exec((err, comments)=>{
+		if(err || !comments){
+			res.status(404).json({msg: err})
+		} else {
+			res.status(200).json({data: comments});
+		}
+	})
+})
+
 router.post('/:url/comment', (req, res)=>{
   req.body.comment = sanitizer(req.body.comment);
   Comment.create(req.body, (err, createdCom)=>{
     err ? console.log(err) : Post.findOne({url: req.params.url}, (err, foundPost)=>{
       if(err){
-				console.log(err);
+				res.status(404).json({message: "Post not found"})
 			}
+			createdCom.post = foundPost._id;
 			foundPost.comments.push(createdCom._id);
       foundPost.save();
-			res.json({message: "Comment saved"});
+			createdCom.postId = foundPost._id;
+			res.status(200).json({message: "Comment saved"});
     })
   })
 })
