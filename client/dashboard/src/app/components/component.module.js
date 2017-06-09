04690aee92fb1bcdb5e307d638080ac41bdf45a6
Ayush Bahuguna <lespaulayush@gmail.com>
2017-06-10 01:47:51 +0530

    add comment component

--- client/dashboard/src/app/components/component.module.js
+++ client/dashboard/src/app/components/component.module.js
@@ -2,5 +2,6 @@ import angular from 'angular';
 import {post} from './post/post.module';
 import {login} from './login/login.module.js';
 import {user} from './user/user.module.js';
+import {comment} from './comment/comment.module';
 
-export const components = angular.module('components', [post, login, user]).name
+export const components = angular.module('components', [post, login, user, comment]).name
