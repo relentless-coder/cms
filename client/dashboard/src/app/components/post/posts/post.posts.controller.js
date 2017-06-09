61ffc4a5c9b1fdc1c4802961cd88e91deca59518
Ayush Bahuguna <lespaulayush@gmail.com>
2017-06-10 01:57:03 +0530

    done

--- client/dashboard/src/app/components/post/posts/post.posts.controller.js
+++ client/dashboard/src/app/components/post/posts/post.posts.controller.js
@@ -2,6 +2,8 @@ import {getPosts, getPostsFunc} from './post.posts.factory';
 
 export function allPostsController(getPosts, $state){
   const ctrl = this;
+  console.log(ctrl);
+  console.log(this);
   ctrl.$onInit = function(){
     getPosts.allPosts().then((data)=>{
       ctrl.posts = data.data.posts;
