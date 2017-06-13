6734920d88591a96e320e1c5249138dfc31fb96d
Ayush Bahuguna <lespaulayush@gmail.com>
2017-06-13 21:43:58 +0530

    lusca

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import http from 'http';
+import lusca from 'lusca';
 import morgan from 'morgan';
 import event from 'events';
 import io from 'socket.io';
@@ -16,57 +17,48 @@ import commentRoutes from './server/routes/comments.routes';
 import authRoutes from './server/routes/auth.routes';
 import userRoutes from './server/routes/users.routes';
 import seedDb from './seed';
-import {passportConfig} from './server/config/passport.config';
-import {socketFunction} from './server/config/socket.io.config';
+import {
+    passportConfig
+} from './server/config/passport.config';
+import {
+    socketFunction
+} from './server/config/socket.io.config';
 import path from 'path';
-
 const logger = morgan(':method :url :status - :response-time ms')
-
 const notifs = [];
-
 const environment = process.env.NODE_ENV || 'development';
-
 let url;
-
-if(environment === 'test'){
-  url = 'mongodb://localhost/blog-test'
-} else if(environment === 'development') {
-  url = 'mongodb://localhost/blog';
-} else if(environment === 'production') {
-  url = process.env.DATABASE_URL
+if (environment === 'test') {
+    url = 'mongodb://localhost/blog-test'
+} else if (environment === 'development') {
+    url = 'mongodb://localhost/blog';
+} else if (environment === 'production') {
+    url = process.env.DATABASE_URL
 }
-
 mongoose.connect(url);
-
 const sanitizer = require('sanitize-html');
-
-
 // Add headers
 const app = express();
 // seedDb();
-
 const localEvent = event.EventEmitter;
 const myEvent = new localEvent();
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Access-Control-Allow-Origin, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-
-  next();
- });
-
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Access-Control-Allow-Origin, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
 app.use(logger);
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(session({
-  secret: "I don't know really",
-  resave: false,
-  saveUninitialized: false
+    secret: "I don't know really",
+    resave: false,
+    saveUninitialized: false
 }))
-
 passportConfig(app);
-
 app.use(postRoutes);
 app.use(commentRoutes);
 app.use(authRoutes);
@@ -74,31 +66,33 @@ app.use(userRoutes);
 app.use(express.static(path.join(__dirname, '/client')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'uploads')))
-
-
+app.use(lusca({
+    csrf: true,
+    xframe: 'SAMEORIGIN',
+    p3p: 'ABCDEF',
+    hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true
+    },
+    xssProtection: true,
+    nosniff: true
+}));
 const port = 6655;
-
 const adminRoutes = ['/admin/', '/admin/:var', '/admin/edit/:var'];
-
-adminRoutes.forEach(function(url){
-  app.get(url, (req, res)=>{
-    res.sendFile(path.join(__dirname, '/client', '/dashboard', '/index.html'));
-  })
+adminRoutes.forEach(function(url) {
+    app.get(url, (req, res) => {
+        res.sendFile(path.join(__dirname, '/client', '/dashboard', '/index.html'));
+    })
 })
-
 const blogRoutes = ['/', '/articles', '/articles/:url', '/contact'];
-
-blogRoutes.forEach(el=>{
-  app.get(el, (req, res)=>{
-    res.sendFile(path.join(__dirname, '/client', '/blog', '/blog.html')); 
-  })
+blogRoutes.forEach(el => {
+    app.get(el, (req, res) => {
+        res.sendFile(path.join(__dirname, '/client', '/blog', '/blog.html'));
+    })
 })
-
-const server = app.listen(process.env.PORT, process.env.IP, ()=>{
-  console.log(`Express server listening on port ${process.env.PORT} and IP ${process.env.IP} and running in ${environment} mode`);
+const server = app.listen(process.env.PORT, process.env.IP, () => {
+    console.log(`Express server listening on port ${process.env.PORT} and IP ${process.env.IP} and running in ${environment} mode`);
 });
-
 var socket = io(server)
 socketFunction(socket, notifs)
-
-
