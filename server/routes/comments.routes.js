62aebfa1a16f98c0b91d06f8e5a360390fc8adc9
Ayush Bahuguna <lespaulayush@gmail.com>
2017-06-13 21:44:33 +0530

    done

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -2,13 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import Post from '../models/post.model';
 import Comment from '../models/comment.model';
-import {decode} from '../config/jwt.token.js';
+import { decode } from '../config/jwt.token.js';
 const sanitizer = require('sanitize-html');
-
-
 
 const app = express();
 const router = express.Router();
+
+/*
+
+	 d888b   d88888b d888888b       .o88b.  .d88b.  .88b  d88. .88b  d88. d88888b d888888b .d8888. 
+	88' Y8b 88'     `~~88~~'      d8P  Y8 .8P  Y8. 88'YbdP`88 88'YbdP`88 88'     `~~88~~' 88'  YP 
+	88      88ooooo    88         8P      88    88 88  88  88 88  88  88 88ooooo    88    `8bo.   
+	88  ooo 88~~~~~    88         8b      88    88 88  88  88 88  88  88 88~~~~~    88      `Y8b. 
+	88. ~8~ 88.        88         Y8b  d8 `8b  d8' 88  88  88 88  88  88 88.        88    db   8D 
+	 Y888P   Y88888P    YP          `Y88P'  `Y88P'  YP  YP  YP YP  YP  YP Y88888P    YP    `8888Y' 
+																								
+								
+*/
 
 router.get('/admin/comment', (req, res) => {
 	if (!req.headers.authorization) {
@@ -17,7 +27,7 @@ router.get('/admin/comment', (req, res) => {
 	const token = req.headers.authorization.split(' ')[1];
 	const payload = decode(token, 'inav');
 	if (payload._id) {
-		Comment.find({}).populate({path: 'post', model: 'Post', select: 'title'}).exec((err, comments) => {
+		Comment.find({}).populate({ path: 'post', model: 'Post', select: 'title' }).exec((err, comments) => {
 			if (err || !comments) {
 				console.log(err);
 				res.status(404).json({ message: err })
@@ -32,43 +42,82 @@ router.get('/admin/comment', (req, res) => {
 	}
 })
 
-router.get('/admin/comment/:id', (req, res)=>{
-	if(!req.headers.authorization){
-		res.status(401).json({message: "You are not logged in"})
+/*
+	
+	 d888b  d88888b  d888888b       .o88b.  .d88b.  .88b  d88. .88b  d88. d88888b d8b   db d888888b 
+	88' Y8b 88'     `~~88~~'      d8P  Y8 .8P  Y8. 88'YbdP`88 88'YbdP`88 88'     888o  88 `~~88~~' 
+	88      88ooooo    88         8P      88    88 88  88  88 88  88  88 88ooooo 88V8o 88    88    
+	88  ooo 88~~~~~    88         8b      88    88 88  88  88 88  88  88 88~~~~~ 88 V8o88    88    
+	88. ~8~ 88.        88         Y8b  d8 `8b  d8' 88  88  88 88  88  88 88.     88  V888    88    
+	 Y888P  Y88888P     YP          `Y88P'  `Y88P'  YP  YP  YP YP  YP  YP Y88888P VP   V8P    YP    
+																								
+																																																													
+*/
+
+router.get('/admin/comment/:id', (req, res) => {
+	if (!req.headers.authorization) {
+		res.status(401).json({ message: "You are not logged in" })
 	}
 
 	const token = req.headers.authorization.split(' ')[1];
 	const payload = decode(token, 'inav');
-	if(payload._id){
-		Comment.findOne({_id: req.params.id}).populate({path: 'post', model: 'Post', select: 'title'}).exec((err, foundComment)=>{
-			if(err || !foundComment){
-				res.status(404).json({message: err})
+	if (payload._id) {
+		Comment.findOne({ _id: req.params.id }).populate({ path: 'post', model: 'Post', select: 'title' }).exec((err, foundComment) => {
+			if (err || !foundComment) {
+				res.status(404).json({ message: err })
 			} else {
-				res.status(200).json({data: foundComment});
+				res.status(200).json({ data: foundComment });
 			}
 		})
 	} else {
-		res.status(404).json({message: "User not found"});
+		res.status(404).json({ message: "User not found" });
 	}
 })
 
-router.delete('/admin/comment/:id', (req, res)=>{
-	if(!req.headers.authorization){
-    	return res.status(401).json({message: 'You are not logged in'})
-  	}
-	Post.findOne({'comments._id': req.params.id}, (err, foundPost)=>{
-		if(err){
+/*
+
+
+	d8888b. d88888b db      d88888b d888888b d88888b       .o88b.  .d88b.  .88b  d88. .88b  d88. d88888b d8b   db d888888b 
+	88  `8D 88'     88      88'     `~~88~~' 88'          d8P  Y8 .8P  Y8. 88'YbdP`88 88'YbdP`88 88'     888o  88 `~~88~~' 
+	88   88 88ooooo 88      88ooooo    88    88ooooo      8P      88    88 88  88  88 88  88  88 88ooooo 88V8o 88    88    
+	88   88 88~~~~~ 88      88~~~~~    88    88~~~~~      8b      88    88 88  88  88 88  88  88 88~~~~~ 88 V8o88    88    
+	88  .8D 88.     88booo. 88.        88    88.          Y8b  d8 `8b  d8' 88  88  88 88  88  88 88.     88  V888    88    
+	Y8888D' Y88888P Y88888P Y88888P    YP    Y88888P       `Y88P'  `Y88P'  YP  YP  YP YP  YP  YP Y88888P VP   V8P    YP    
+                                                                                                                       
+                                                                                                                       
+
+ */
+
+router.delete('/admin/comment/:id', (req, res) => {
+	if (!req.headers.authorization) {
+		return res.status(401).json({ message: 'You are not logged in' })
+	}
+	Post.findOne({ 'comments._id': req.params.id }, (err, foundPost) => {
+		if (err) {
 			res.status(404).json(err);
 		}
 		const index = foundPost.comments.indexOf(req.params.id);
 		foundPost.comments.splice(index, 1);
-		foundPost.save((err, data)=>{
-			Comment.remove({_id: req.params.id}, (err, result)=>{
-				res.status(200).json({message: "Comment deleted"})
+		foundPost.save((err, data) => {
+			Comment.remove({ _id: req.params.id }, (err, result) => {
+				res.status(200).json({ message: "Comment deleted" })
 			})
 		})
 	})
 })
+
+/*
+
+	d8888b.  .d88b.  .d8888. d888888b       .o88b.  .d88b.  .88b  d88. .88b  d88. d88888b d8b   db d888888b 
+	88  `8D .8P  Y8. 88'  YP `~~88~~'      d8P  Y8 .8P  Y8. 88'YbdP`88 88'YbdP`88 88'     888o  88 `~~88~~' 
+	88oodD' 88    88 `8bo.      88         8P      88    88 88  88  88 88  88  88 88ooooo 88V8o 88    88    
+	88~~~   88    88   `Y8b.    88         8b      88    88 88  88  88 88  88  88 88~~~~~ 88 V8o88    88    
+	88      `8b  d8' db   8D    88         Y8b  d8 `8b  d8' 88  88  88 88  88  88 88.     88  V888    88    
+	88       `Y88P'  `8888Y'    YP          `Y88P'  `Y88P'  YP  YP  YP YP  YP  YP Y88888P VP   V8P    YP    
+																											
+																											
+
+*/
 
 router.post('/:url/comment', (req, res) => {
 	req.body.comment = sanitizer(req.body.comment);
@@ -86,7 +135,32 @@ router.post('/:url/comment', (req, res) => {
 	})
 })
 
+/*
+
+	d8888b. d88888b d8888b. db      db    db       .o88b.  .d88b.  .88b  d88. .88b  d88. d88888b d8b   db d888888b 
+	88  `8D 88'     88  `8D 88      `8b  d8'      d8P  Y8 .8P  Y8. 88'YbdP`88 88'YbdP`88 88'     888o  88 `~~88~~' 
+	88oobY' 88ooooo 88oodD' 88       `8bd8'       8P      88    88 88  88  88 88  88  88 88ooooo 88V8o 88    88    
+	88`8b   88~~~~~ 88~~~   88         88         8b      88    88 88  88  88 88  88  88 88~~~~~ 88 V8o88    88    
+	88 `88. 88.     88      88booo.    88         Y8b  d8 `8b  d8' 88  88  88 88  88  88 88.     88  V888    88    
+	88   YD Y88888P 88      Y88888P    YP          `Y88P'  `Y88P'  YP  YP  YP YP  YP  YP Y88888P VP   V8P    YP    
+                                                                                                               
+                                                                                                               
+
+*/
+
 router.post('/comment/:commentId', (req, res) => {
+	if (req.headers.authorization) {
+		const token = req.headers.authorization.split(' ')[1];
+		const payload = decode(token, 'inav');
+		User.findById(payload._id, (err, foundUser) => {
+			const author = {
+				name: foundUser.name,
+				email: foundUser.email
+			}
+			req.body.author = author;
+		})
+	}
+
 	req.body.comment = sanitizer(req.body.comment);
 	Comment.create(req.body, (err, createdCom) => {
 		err ? res.json({ error: 'message' }) : Comment.findById(req.params.commentId, (err, foundComment) => {
@@ -95,6 +169,7 @@ router.post('/comment/:commentId', (req, res) => {
 			res.json({ message: "Comment reply saved" })
 		})
 	})
+
 })
 
 export default router;
