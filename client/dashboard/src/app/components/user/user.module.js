b1759fe96ec991d3376b2f25c6ef5a89cae0890a
Ayush Bahuguna <lespaulayush@gmail.com>
2017-07-18 08:25:08 +0530

    user scss file

--- client/dashboard/src/app/components/user/user.module.js
+++ client/dashboard/src/app/components/user/user.module.js
@@ -4,6 +4,7 @@ import ngFile from  'ng-file-upload';
 import 'ngstorage';
 import {userFactory, userFactoryFunc} from './user.factory.js'
 import {userComponent, userComponentOptions} from './user.component.js'
+import './user.scss';
 
 export const user = angular.module('user', [uiRouter, ngFile, 'ngStorage'])
 .component(userComponent, userComponentOptions)
