8b8a8060812bf99feeb83e570d0b021eb262f6c4
relentless-coder <contact@ayushbahuguna.com>
2017-08-02 08:34:45 +0530

    make secret internal

--- server/config/jwt.token.js
+++ server/config/jwt.token.js
@@ -1,6 +1,9 @@
 import crypto from 'crypto';
+import {
+  config
+} from './package-config';
 
-export const encode = function(payload, secret){
+export const encode = function(payload) {
   const algorithm = 'HS256';
   const header = {
     typ: 'JWT',
@@ -9,33 +12,37 @@ export const encode = function(payload, secret){
 
   let jwt = `${base64encode(JSON.stringify(header))}.${base64encode(JSON.stringify(payload))}.`;
 
-  jwt += sign(jwt, secret);
+  jwt += sign(jwt, config.secret);
   return jwt;
 }
 
-export const decode = function(token){
-  if(!token){
-    throw new Error('No token received')
-    
-  }
+export const decode = function(req) {
+
+  const token = req.headers.authorization.split(' ')[1];
   const segments = token.split('.')
-  if(segments.length !== 3){
+  if (segments.length !== 3) {
     throw new Error('Invalid token');
   }
-  const header = JSON.parse(base64decode(segments[0]));
-  const payload = JSON.parse(base64decode(segments[1]));
-  const signature = `${segments}.${payload}`
-  return payload;
+  const header = segments[0];
+  const payload = segments[1];
+  const tokenSign = segments[2];
+  const signature = `${header}.${payload}.`;
+  let jwt = sign(signature, config.secret);
+  if (jwt === tokenSign) {
+    return JSON.parse(base64decode(segments[1]))
+  } else {
+    throw new Error('Invalid token');
+  }
 }
 
-function base64encode(str){
+function base64encode(str) {
   return new Buffer(str).toString('base64');
 }
 
-function base64decode(str){
+function base64decode(str) {
   return new Buffer(str, 'base64').toString();
 }
 
-function sign(str, key){
-  return crypto.createHmac('sha256', key).update(str).digest('base64');
+function sign(str) {
+  return crypto.createHmac('sha256', config.secret).update(str).digest('base64');
 }
