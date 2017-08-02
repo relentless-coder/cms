9c6ddc8e5073f1ff3edb876037aaedba35d1df85
relentless-coder <contact@ayushbahuguna.com>
2017-08-02 08:42:22 +0530

    import optin module

--- client/blog/src/app/root.module.js
+++ client/blog/src/app/root.module.js
@@ -1,12 +1,13 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import {header} from './common/header/header.module.js';
-import {navbar} from './common/navbar/navbar.module.js';
-import {components} from './components/components.module.js'
+import {header} from './common/header/header.module';
+import {navbar} from './common/navbar/navbar.module';
+import {optin} from './common/optin/optin.module'
+import {components} from './components/components.module'
 import {rootComponentName, rootComponent} from './root.component';
 import './root.scss';
 
-angular.module('blog', [uiRouter, header, navbar, components])
+angular.module('blog', [uiRouter, header, navbar, optin, components])
 .component(rootComponentName, rootComponent)
 .config(['$stateProvider', '$locationProvider', ($stateProvider, $locationProvider)=>{
   $locationProvider.html5Mode(true);
