48779e6f67d431f49499edc181d897f81e600f6e
relentless-coder <contact@ayushbahuguna.com>
2017-08-02 08:38:22 +0530

    done

--- client/blog/src/app/components/home/home.controller.js
+++ client/blog/src/app/components/home/home.controller.js
@@ -1,9 +1,8 @@
 function homeController($localStorage){
   const ctrl = this;
   ctrl.$onInit = function(){
-    if($localStorage.user){
+    
       ctrl.about = $localStorage.user.about;
-    }
   }
 }
 
