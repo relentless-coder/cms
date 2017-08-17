1943bcdfbcd6faf07f4fe3b52527278523ef86de
relentless-coder <contact@ayushbahuguna.com>
2017-08-17 23:31:29 +0530

    inject userFactory to the controller

--- client/blog/src/app/components/home/home.component.js
+++ client/blog/src/app/components/home/home.component.js
@@ -3,6 +3,6 @@ import {homeController} from './home.controller'
 export const homeComponent = 'homeComponent'
 export const homeComponentOptions = {
   templateUrl: 'blog/src/app/components/home/home.html',
-  controller: ['$localStorage', homeController],
+  controller: ['userFactory', '$localStorage', homeController],
   controllerAs: 'ctrl'
 }
