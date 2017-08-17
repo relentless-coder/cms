483fb3df413866ccfcdb004048aededb0adf0f8f
relentless-coder <contact@ayushbahuguna.com>
2017-08-17 23:34:08 +0530

    setup nodemailer with ses

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mailer from 'nodemailer';
 import multer from 'multer';
 import fs from 'fs';
 import User from '../models/user.model';
 import Subscriber from '../models/subscriber.model';
 import { decode } from '../config/jwt.token.js'
+import { config } from '../config/package-config';
 
 let notifs = [];
 
@@ -14,28 +16,47 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		if(!file){
+		if (!file) {
 			return;
 		}
 		cb(null, 'uploads');
 	},
 	filename: function (req, file, cb) {
-		if(!file){
+		if (!file) {
 			return;
 		}
 		cb(null, file.fieldname + '-' + Date.now() + '.png')
 	}
 });
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage })
+
+let transporter = mailer.createTransport({
+	host: config.host,
+	port: config.port,
+	secure: true, // secure:true for port 465, secure:false for port 587
+	auth: {
+		user: config.mailUser,
+		pass: config.mailPass
+	}
+});
+
+transporter.verify(function (error, success) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Server is ready to take our messages');
+	}
+});
+
 
 router.get('/admin/user', (req, res) => {
 	if (!req.headers.authorization) {
 		res.status(401).json({ message: 'You are not logged in' })
 	}
-	
+
 	const payload = decode(req);
-	
+
 	if (payload._id) {
 		User.findById(payload._id)
 			.populate({ path: 'posts', model: 'Post' })
@@ -74,41 +95,61 @@ router.put('/admin/user', upload.single('file'), (req, res, next) => {
 	}
 });
 
-router.get('/notifs', (req, res)=>{
+router.get('/notifs', (req, res) => {
 	if (!req.headers.authorization) {
 		res.status(401).json({ message: 'You are not logged in' })
 	}
 	const payload = decode(req);
 	if (payload._id) {
-		res.status(200).json({notifs: notifs})
+		res.status(200).json({ notifs: notifs })
 		notifs = [];
 	} else {
-		res.status(404).json({message: 'user doesn\'t exist'})
+		res.status(404).json({ message: 'user doesn\'t exist' })
 	}
 })
 
 router.post('/contact', (req, res) => {
-	User.find({}, (err, users) => {
-		const user = users[0];
-		user.queries.push(req.body)
-		user.save()
-		console.log(user);
-		res.status(200).json({ message: 'Query submitted' })
+	let options = {
+		from: req.body.email,
+		to: 'contact@ayushbahuguna.com',
+		subject: `${req.body.name} has a message for you`,
+		text: req.body.message
+	}
+
+	transporter.sendMail(options, (err, success)=>{
+		if(err){
+			res.status(500).json({message: err})
+		} else {
+			res.status(200).json({message: 'Query submitted'})
+		}
 	})
 })
 
-router.post('/subscribe', (req, res)=>{
-	Subscriber.create(req.body).then((data)=>{
-		res.status(200).json({message: 'Success'})
-	}).catch((err)=>{
-		res.status(500).json({message: err})
+router.post('/subscribe', (req, res) => {
+	let options = {
+		from: 'contact@ayushbahuguna.com',
+		to: req.body.email,
+		subject: `Welcome, ${req.body.name}`,
+		text: 'Thank you subscribing to the notifications from my blog.'
+	}
+	Subscriber.create(req.body).then((data) => {
+		transporter.sendMail(options, (err, success) => {
+			if (err) {
+				console.log(err)
+			} else {
+				res.status(200).json({ message: 'Success' })
+
+			}
+		})
+	}).catch((err) => {
+		res.status(500).json({ message: err })
 	})
 })
 
 export default router;
 
-function returnNotifs(value){
+function returnNotifs(value) {
 	notifs.push(value)
 }
 
-export {returnNotifs}
+export { returnNotifs }
