c5be6a07b22636a9bc7ffdcb9caf53118dd4ed5a
relentless-coder <contact@ayushbahuguna.com>
2017-08-18 01:30:33 +0530

    fix typo in the message

--- client/blog/src/app/common/optin/optin.controller.js
+++ client/blog/src/app/common/optin/optin.controller.js
@@ -3,7 +3,7 @@ function optinFunction(optinFactory) {
 	const ctrl = this;
 	ctrl.subscriber = {};
 	ctrl.info = {};
-	ctrl.info.message = 'Hey, if you enjoyed this post, then I can drop you an email everytime I write a new post. Your information will not shared with anyone.';
+	ctrl.info.message = 'Hey, if you enjoyed this post, then I can drop you an email everytime I write a new post. Your information will not be shared with anyone.';
 
 	ctrl.subscribe = function subscribe() {
 		if (ctrl.subscriber) {
