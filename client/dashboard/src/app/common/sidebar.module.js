42a7e8c625c506ef3e81df2ef96b0b4b1dd6a6e9
relentless-coder <contact@ayushbahuguna.com>
2017-08-18 01:32:02 +0530

    don't initialize the same tokenFactory again for this module

--- client/dashboard/src/app/common/sidebar.module.js
+++ client/dashboard/src/app/common/sidebar.module.js
@@ -3,7 +3,6 @@ import uiRouter from 'angular-ui-router';
 import 'ngstorage';
 import {removeNotifs} from './notifs.directive';
 import {showComments} from './show.comments.directive';
-import {tokenFactory, tokenFactoryFunc} from '../components/login/token.factory.js'
 import {notifsFactory, notifsFactoryFunc} from './notifs.factory'
 import {sidebarComponentName, sidebarComponent} from './sidebar.component';
 import {sidebarController} from './sidebar.controller';
@@ -12,7 +11,6 @@ import './sidebar.scss';
 export const sidebar = angular.module('sidebar', [uiRouter, 'ngStorage'])
 .component(sidebarComponentName, sidebarComponent)
 .directive('removeNotifs', removeNotifs)
-.factory(tokenFactory, ['$localStorage', tokenFactoryFunc])
 .factory(notifsFactory, ['$http', notifsFactoryFunc])
 .directive('showComments', showComments)
 .name;
