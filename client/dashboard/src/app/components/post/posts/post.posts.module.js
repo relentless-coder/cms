74936f6e0d102d0182a233ac9407418a7c045e07
relentless-coder <contact@ayushbahuguna.com>
2017-08-18 01:35:45 +0530

    inject dependency to the config block || don't initialize tokenFactory again for this module

--- client/dashboard/src/app/components/post/posts/post.posts.module.js
+++ client/dashboard/src/app/components/post/posts/post.posts.module.js
@@ -4,11 +4,10 @@ import ngSanitize from 'angular-sanitize';
 import 'ngstorage';
 import {getPosts, getPostsFunc} from './post.posts.factory';
 import {allPostsComponent, allPostsOptions} from './post.posts.component'
-import {tokenFactory, tokenFactoryFunc} from '../../login/token.factory.js'
 
 export const posts = angular.module('post.posts', [uiRouter, ngSanitize, 'ngStorage'])
 .component(allPostsComponent, allPostsOptions)
-.config(function($stateProvider, $urlRouterProvider){
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
   const homeState = {
     name: 'home',
     url: '/admin/home',
@@ -16,7 +15,6 @@ export const posts = angular.module('post.posts', [uiRouter, ngSanitize, 'ngStor
   }
   $stateProvider.state(homeState);
   $urlRouterProvider.when('/admin/', '/admin/home')
-})
+}])
 .factory(getPosts, ['$http', getPostsFunc])
-.factory(tokenFactory, ['$localStorage', tokenFactoryFunc])
 .name
