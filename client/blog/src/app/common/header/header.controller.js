d5c0fa75914798073eb3f6df0ceb67a9ad4d008b
relentless-coder <contact@ayushbahuguna.com>
2017-08-17 23:34:56 +0530

    don't use localStorage

--- client/blog/src/app/common/header/header.controller.js
+++ client/blog/src/app/common/header/header.controller.js
@@ -1,15 +1,10 @@
 function headerController(userFactory, $localStorage){
   const ctrl = this;
   ctrl.$onInit = function(){
-    if($localStorage.user){
-      ctrl.user = $localStorage.user;
-    } else {
        userFactory.getUserInfo().then((data)=>{
         console.log(data);
         ctrl.user = data.data.user;
-        $localStorage.user = data.data.user;
       });
-    }
   };
 }
 
