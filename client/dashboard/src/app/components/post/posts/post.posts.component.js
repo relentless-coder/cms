d7a3d7b1e43e3967a2c642d49abed709e69f8c61
relentless-coder <contact@ayushbahuguna.com>
2017-08-18 01:33:35 +0530

    inject dependency to the config block

--- client/dashboard/src/app/components/post/posts/post.posts.component.js
+++ client/dashboard/src/app/components/post/posts/post.posts.component.js
@@ -1,9 +1,8 @@
-import {getPosts, getPostsFunc} from './post.posts.factory';
 import {allPostsController} from './post.posts.controller';
 
 export const allPostsComponent = 'allPosts';
 export const allPostsOptions = {
   templateUrl: '/dashboard/src/app/components/post/posts/post.posts.html',
-  controller: [getPosts, '$state', 'tokenFactory', allPostsController],
+  controller: ['getPosts', '$state', 'tokenFactory', allPostsController],
   controllerAs: 'ctrl'
 }
