d62bf405ce017231a3987772f47d1a198e2f6f2a
relentless-coder <contact@ayushbahuguna.com>
2017-08-21 00:23:59 +0530

    configure local-signup strategy to check for already existing user, then return error

--- server/config/strategies/local.strategy.js
+++ server/config/strategies/local.strategy.js
@@ -3,50 +3,67 @@ const LocalStrategy = require('passport-local').Strategy;
 import User from '../../models/user.model';
 
 
-export function localConfig(){
+export function localConfig() {
   passport.use('local-signup', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
-    passReqToCallback : true
-  }, function(req, username, password, done){
-     User.findOne({email: username}, (err, user)=>{
-		if(err){
-      return done(err);
-    } else if(user){
-      return done(null, false, {
-        message: "This email is already in use"
-      })
-    } else if(!user){
-      const newUser = new User({email: username});
-      newUser.password = newUser.generateHash(password);
-      newUser.save(function(err){
-        if(err){
-          return done(err);
+    passReqToCallback: true
+  }, function(req, username, password, done) {
+    User.find({})
+      .then((data) => {
+        if (data.length > 0) {
+          console.log('we already have a user')
+          return done(null, false, {
+            message: 'Sorry, can\'t have more than one user.'
+          })
         } else {
-          return done(null, newUser)
+          User.findOne({
+            email: username
+          }, (err, user) => {
+            if (err) {
+              return done(err);
+            } else if (user) {
+              return done(null, false, {
+                message: 'This email is already in use'
+              })
+            } else if (!user) {
+              const newUser = new User({
+                email: username
+              });
+              newUser.password = newUser.generateHash(password);
+              newUser.save(function(err) {
+                if (err) {
+                  return done(err);
+                } else {
+                  return done(null, newUser)
+                }
+              })
+            }
+          })
         }
       })
-    }
-	})
+
   }))
 
   passport.use('local-login', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-  }, function(req, username, password, done){
-    User.findOne({email: username}, (err, user)=>{
-      if(err){
+  }, function(req, username, password, done) {
+    User.findOne({
+      email: username
+    }, (err, user) => {
+      if (err) {
         return done(err);
-      }else if(!user){
+      } else if (!user) {
         return done(null, false, {
           message: "Invalid email"
         })
-      } else if(!user.validPassword(user, password)){
+      } else if (!user.validPassword(user, password)) {
         return done(null, false, {
           message: "Invalid Password"
         })
-      } else if(user){
+      } else if (user) {
         return done(null, user)
       }
     })
