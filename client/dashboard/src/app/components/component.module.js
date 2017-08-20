13d863f26533d666ca5d4af7b741836e0bf4534a
relentless-coder <contact@ayushbahuguna.com>
2017-08-21 00:25:49 +0530

    inject signup module

--- client/dashboard/src/app/components/component.module.js
+++ client/dashboard/src/app/components/component.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import {post} from './post/post.module';
 import {login} from './login/login.module.js';
 import {user} from './user/user.module.js';
+import {signup} from './signup/signup.module.js';
 import {comment} from './comment/comment.module';
 
-export const components = angular.module('components', [post, login, user, comment]).name
+export const components = angular.module('components', [post, login, user, comment, signup]).name
