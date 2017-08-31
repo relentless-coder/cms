a0e363a605a1fac0bb6e4337e4c38fa30cc075ac
relentless-coder <contact@ayushbahuguna.com>
2017-09-01 00:47:12 +0530

    add interests path

--- server/models/subscriber.model.js
+++ server/models/subscriber.model.js
@@ -3,7 +3,8 @@ import {Schema} from 'mongoose';
 
 const subscriberSchema = new Schema({
     email: String,
-    name: String
+    name: String,
+    interests: Array
 })
 
 export default mongoose.model('Subscriber', subscriberSchema)
