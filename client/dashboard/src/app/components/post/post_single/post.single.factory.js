664ccace576e16fd361fa96bd084553bfab15b5d
relentless-coder <contact@ayushbahuguna.com>
2017-09-07 22:08:47 +0530

    remove unnecessary dependency

--- client/dashboard/src/app/components/post/post_single/post.single.factory.js
+++ client/dashboard/src/app/components/post/post_single/post.single.factory.js
@@ -1,5 +1,5 @@
 export const postSingleFactory = 'postSingleFactory';
-export const postSingleFactoryFunc = function($http, $stateParams){
+export const postSingleFactoryFunc = function($http){
   return {
     getSinglePost: function(value){
       return $http.get('/post/' + value);
