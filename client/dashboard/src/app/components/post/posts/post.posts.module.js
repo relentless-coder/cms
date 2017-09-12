5f45bfa46838d57e6cf3154f06e77e36f5f7d2a9
relentless-coder <contact@ayushbahuguna.com>
2017-09-12 23:54:44 +0530

    include togglePost directive

--- client/dashboard/src/app/components/post/posts/post.posts.module.js
+++ client/dashboard/src/app/components/post/posts/post.posts.module.js
@@ -4,8 +4,10 @@ import ngSanitize from 'angular-sanitize';
 import 'ngstorage';
 import {getPosts, getPostsFunc} from './post.posts.factory';
 import {allPostsComponent, allPostsOptions} from './post.posts.component'
+import {togglePost} from './post.posts.directive';
 
 export const posts = angular.module('post.posts', [uiRouter, ngSanitize, 'ngStorage'])
+.directive('togglePost', togglePost)
 .component(allPostsComponent, allPostsOptions)
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
   const homeState = {
