a24858dfefb197565294cad3c4020feb7564ca20
relentless-coder <contact@ayushbahuguna.com>
2017-09-12 23:55:42 +0530

    include scss file

--- client/dashboard/src/app/components/post/post_new/post.new.module.js
+++ client/dashboard/src/app/components/post/post_new/post.new.module.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import {thisPost, thisPostFunc} from './post.new.factory';
 import {postNewComponent, postNewOptions} from './post.new.component';
+import './post.new.scss';
 
 export const newPost = angular.module('post.new', [uiRouter, 'ui.tinymce'])
 .component(postNewComponent, postNewOptions)
