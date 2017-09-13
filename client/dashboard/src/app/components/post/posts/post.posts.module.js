e8a1236c53fdc5e23f04ab7621fac85a7cd2e3a0
relentless-coder <contact@ayushbahuguna.com>
2017-09-13 23:27:52 +0530

    include the scss file and the directive

--- client/dashboard/src/app/components/post/posts/post.posts.module.js
+++ client/dashboard/src/app/components/post/posts/post.posts.module.js
@@ -2,21 +2,22 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngSanitize from 'angular-sanitize';
 import 'ngstorage';
-import {getPosts, getPostsFunc} from './post.posts.factory';
-import {allPostsComponent, allPostsOptions} from './post.posts.component'
-import {togglePost} from './post.posts.directive';
+import { getPosts, getPostsFunc } from './post.posts.factory';
+import { allPostsComponent, allPostsOptions } from './post.posts.component'
+import { togglePost } from './post.posts.directive';
+import './post.posts.scss';
 
 export const posts = angular.module('post.posts', [uiRouter, ngSanitize, 'ngStorage'])
-.directive('togglePost', togglePost)
-.component(allPostsComponent, allPostsOptions)
-.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-  const homeState = {
-    name: 'home',
-    url: '/admin/home',
-    component: allPostsComponent
-  }
-  $stateProvider.state(homeState);
-  $urlRouterProvider.when('/admin/', '/admin/home')
-}])
-.factory(getPosts, ['$http', getPostsFunc])
-.name
+  .directive('togglePost', togglePost)
+  .component(allPostsComponent, allPostsOptions)
+  .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    const homeState = {
+      name: 'home',
+      url: '/admin/home',
+      component: allPostsComponent
+    }
+    $stateProvider.state(homeState);
+    $urlRouterProvider.when('/admin/', '/admin/home')
+  }])
+  .factory(getPosts, ['$http', getPostsFunc])
+  .name
