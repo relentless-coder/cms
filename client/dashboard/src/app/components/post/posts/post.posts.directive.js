a2061561fe63819518112624318d073a9b9a7a54
relentless-coder <contact@ayushbahuguna.com>
2017-09-13 23:28:58 +0530

    toggle active class on the element

--- client/dashboard/src/app/components/post/posts/post.posts.directive.js
+++ client/dashboard/src/app/components/post/posts/post.posts.directive.js
@@ -3,6 +3,8 @@ export function togglePost(){
     restrict: 'A',
     link: function(scope, element, attrs){
       element.on('click', ()=>{
+        $('.toggle_button').removeClass('active')
+        $(element).addClass('active');
         $('.post-item').addClass('hide');
         let id = $(element).attr('id');
         $(`.post-item.${id}`).removeClass('hide');
