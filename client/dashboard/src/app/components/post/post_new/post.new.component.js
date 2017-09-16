069f4b72f72c0d8244deb5bf88eb4b890dce2f2f
relentless-coder <contact@ayushbahuguna.com>
2017-09-16 08:40:20 +0530

    inject  as the dependency to the controller

--- client/dashboard/src/app/components/post/post_new/post.new.component.js
+++ client/dashboard/src/app/components/post/post_new/post.new.component.js
@@ -3,7 +3,7 @@ import {postNewController} from './post.new.controller'
 
 export const postNewOptions = {
   templateUrl: 'dashboard/src/app/components/post/post_new/post.new.html',
-  controller: ['thisPost', '$http', postNewController],
+  controller: ['thisPost', '$http', '$state', postNewController],
   controllerAs: 'ctrl'
 }
 
