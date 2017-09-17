3bbe2771bceb5e100673c75531984a27f4d52b20
relentless-coder <contact@ayushbahuguna.com>
2017-09-18 00:03:05 +0530

    remove console log

--- client/blog/src/app/common/header/header.controller.js
+++ client/blog/src/app/common/header/header.controller.js
@@ -2,7 +2,6 @@ function headerController(userFactory, $localStorage){
   const ctrl = this;
   ctrl.$onInit = function(){
        userFactory.getUserInfo().then((data)=>{
-        console.log(data);
         ctrl.user = data.data.user;
       });
   };
