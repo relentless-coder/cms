727211e6fa16cd0b70546b37aeadf33b850ffe9e
relentless-coder <contact@ayushbahuguna.com>
2017-09-18 00:00:12 +0530

    add newsletter component

--- client/dashboard/src/app/components/component.module.js
+++ client/dashboard/src/app/components/component.module.js
@@ -4,5 +4,6 @@ import {login} from './login/login.module.js';
 import {user} from './user/user.module.js';
 import {signup} from './signup/signup.module.js';
 import {comment} from './comment/comment.module';
+import {news} from './newsletter/newsletter.module';
 
-export const components = angular.module('components', [post, login, user, comment, signup]).name
+export const components = angular.module('components', [post, login, user, comment, signup, news]).name
