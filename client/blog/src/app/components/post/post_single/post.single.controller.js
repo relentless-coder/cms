72de54c34372060f6916e831b748c9ba6573adcc
relentless-coder <contact@ayushbahuguna.com>
2017-10-02 15:28:35 +0530

    extract out post content into a variable

--- client/blog/src/app/components/post/post_single/post.single.controller.js
+++ client/blog/src/app/components/post/post_single/post.single.controller.js
@@ -9,6 +9,7 @@ function postSingleController(postSingleFactory, $stateParams, $rootScope) {
     postSingleFactory.getPost($stateParams.url).then((data) => {
       ctrl.post = data.data.post;
       ctrl.post.comments.forEach(el => el.replyVisible = false);
+      ctrl.content = ctrl.post.content;
     })
   }
 
