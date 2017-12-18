5c3dccc0e066af0dbd2ba01f6a7d0405373bc3f1
relentless-coder <contact@ayushbahuguna.com>
2017-12-18 08:29:48 +0530

    update new credentials

---
+++ server/config/package-config.js
@@ -0,0 +1,9 @@
+export const config = {
+    secret: process.env.SECRET,
+    host: process.env.mailHost,
+    port: process.env.mailPort,
+    mailUser: process.env.mailUser,
+    mailPassword: process.env.mailPassword
+}
+
+console.log(config)
