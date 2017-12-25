633a8c754118a19497b0e30a1eb5f2f0be0cd66e
relentless-coder <contact@ayushbahuguna.com>
2017-12-25 21:23:29 +0530

    inject

--- client/blog/src/app/components/post/posts/posts.component.js
+++ client/blog/src/app/components/post/posts/posts.component.js
@@ -3,6 +3,6 @@ import {postsController} from './posts.controller'
 export const postsComponent = 'postsComponent'
 export const postsComponentOptions = {
   templateUrl: 'blog/src/app/components/post/posts/posts.html',
-  controller: ['$localStorage', 'postsFactory', postsController],
+  controller: ['$localStorage', 'postsFactory', '$rootScope', postsController],
   controllerAs: 'ctrl'
 }
