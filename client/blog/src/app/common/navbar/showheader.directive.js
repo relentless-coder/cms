fcffb2a1c12ac0cbd80a8c9abafdf24df43c734e
relentless-coder <contact@ayushbahuguna.com>
2017-12-25 21:34:56 +0530

    animate the opacity 1 -> 0 of the header and scroll to the view section

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -1,16 +1,23 @@
-function showHeader(){
-    return  {
+function showHeader() {
+    return {
         restrict: 'A',
-        link: function(scope, element, attrs){
-            element.on('click', function(){
-                document.querySelector('.header').classList.remove('zero_height');
-                document.querySelector('.posts').classList.remove('hide');
-                setTimeout(()=>{
-                    document.querySelector('.nav-wrapper').classList.remove('fixed_nav')
-                }, 500)
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                if (!$('.nav-wrapper.fixed_nav').length) {
+                    $('html, body').animate({
+                        scrollTop: $('.view-section').offset().top - 60
+                    }, 1100);
+                    $('.profile-wrapper, .name-wrapper, .social-wrapper').animate({
+                        opacity: 0
+                    }, 1200, () => {
+                        $('.header').addClass('hide');
+                        $('html, body').scrollTop(0);
+                        $('.nav-wrapper').addClass('.fixed_nav')
+                    })
+                }
             })
         }
     }
 }
 
-export {showHeader}
+export { showHeader }
