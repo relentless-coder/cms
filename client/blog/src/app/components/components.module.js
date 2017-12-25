f41f6aa1de3e9c1592358e59d1e608d21a66e118
relentless-coder <contact@ayushbahuguna.com>
2017-12-25 21:33:40 +0530

    include the about module

--- client/blog/src/app/components/components.module.js
+++ client/blog/src/app/components/components.module.js
@@ -1,6 +1,6 @@
-import {home} from './home/home.module'
+import {about} from './about/about.module'
 import {contact} from './contact/contact.module'
 import {post} from './post/post.module'
 
-export const components = angular.module('components', [home, contact, post])
+export const components = angular.module('components', [about, contact, post])
 .name
