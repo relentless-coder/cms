7e26935bae08720aacd33766e9b3bb9fb4306854
relentless-coder <contact@ayushbahuguna.com>
2017-12-25 21:25:02 +0530

    add scrollHide directive

--- client/blog/src/app/components/post/post_single/post.single.module.js
+++ client/blog/src/app/components/post/post_single/post.single.module.js
@@ -1,10 +1,12 @@
 import {postSingleFactory, postSingleFactoryFunc} from './post.single.factory'
 import {postSingleComponent, postSingleComponentOptions} from './post.single.component'
-import {highlightDirective} from './post.single.directive';
+import {highlightDirective} from './highlight.directive';
+import {scrollHide} from './header.scroll.directive';
 import './post.single.scss'
 
 export const post_single = angular.module('postSingle', [])
 .directive('singlePostContent', ['hljsService', '$window', highlightDirective])
+.directive('scrollHide', ['$window', '$document', scrollHide])
 .component(postSingleComponent, postSingleComponentOptions)
 .factory(postSingleFactory, ['$http', postSingleFactoryFunc])
 .name
