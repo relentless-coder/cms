9695064dcd356d4adb96f0f2a8ea988499b0f218
relentless-coder <contact@ayushbahuguna.com>
2017-12-25 20:48:37 +0530

    add meta content for the home page

--- client/blog/src/app/root.module.js
+++ client/blog/src/app/root.module.js
@@ -8,5 +8,14 @@ import './root.scss';
 
 angular.module('blog', [header, navbar, optin, components, 'ui.router', 'ngFileUpload','ngSanitize', 'ngStorage', 'hljs'])
 .component(rootComponentName, rootComponent)
-.config(['$stateProvider', '$locationProvider', appConfig])
+.config(['$stateProvider', '$locationProvider',  appConfig])
+.run(['$rootScope', ($rootScope)=>{
+    const meta = {
+        keywords: 'Ayush Bahuguna, mean stack developer, javascript tutorials, nodejs tutorials, javascript blog, nodejs blog, backend developer tutorials',
+        description: 'I am currently employed as a full stack javscript developer, and I am always looking for freelance work. This blog serves as a platform for me to keep track of my progress in becoming a better developer. The articles posted here are open to discussion.',
+        title: 'Ayush Bahuguna | Full Stack Javascript Developer'
+      }
+    
+      $rootScope.meta = meta;
+}])
 .name;
