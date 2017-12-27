51067ae73b71ac3b01f9d8d570e64ec37b64e0a2
relentless-coder <contact@ayushbahuguna.com>
2017-12-27 08:36:00 +0530

    add/remove hide based on state

--- client/blog/src/app/common/header/header.controller.js
+++ client/blog/src/app/common/header/header.controller.js
@@ -1,6 +1,11 @@
-function headerController(userFactory, $localStorage){
+function headerController(userFactory, $localStorage, $state){
   const ctrl = this;
+
   ctrl.$onInit = function(){
+       console.log("hello world");
+       if($state.current.name === 'home' || $state.current.name === 'about'){
+         $('.header').removeClass('hide')
+       }
        userFactory.getUserInfo().then((data)=>{
         ctrl.user = data.data.user;
       });
