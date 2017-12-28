dc781274362ba7fb3b7527897997e4d063148fc8
relentless-coder <contact@ayushbahuguna.com>
2017-12-28 08:44:15 +0530

    check if has property hide

--- client/blog/src/app/components/post/post_single/header.scroll.directive.js
+++ client/blog/src/app/components/post/post_single/header.scroll.directive.js
@@ -2,17 +2,19 @@ function scrollHide($window, $document) {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-            $document.on('scroll', ()=>{
-                if($window.scrollY > 500){
+            $document.on('scroll', () => {
+                if ($window.scrollY > 900 && !$(element).hasClass('hide')) {
                     angular.element(element).addClass('hide')
                     angular.element(document.querySelector('.single_post_content_wrapper')).addClass('no_z_index')
-                } else {
+
+                } else if ($window.scrollY <= 900 && $(element).hasClass('hide')) {
+
                     angular.element(element).removeClass('hide')
-                    angular.element(document.querySelector('.single_post_content_wrapper')).removeClass('no_z_index')                    
+                    angular.element(document.querySelector('.single_post_content_wrapper')).removeClass('no_z_index');
                 }
             })
         }
     }
 }
 
-export {scrollHide}
+export { scrollHide }
