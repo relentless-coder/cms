ec169aca5c4fef99472f87477d7f1dccc850335c
relentless-coder <contact@ayushbahuguna.com>
2017-12-28 08:42:10 +0530

    check if div has a directive

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -4,14 +4,15 @@ function showHeader() {
         link: function (scope, element, attrs) {
             element.on('click', function () {
                 if (!$('.nav-wrapper.fixed_nav').length) {
-                    $('html, body').animate({
-                        scrollTop: $('.home').offset().top - 64
+                    $('html,body').animate({
+                        scrollTop: $('.view-section').offset().top
                     }, 1100);
                     $('.profile-wrapper, .name-wrapper, .social-wrapper').animate({
                         opacity: 0
                     }, 1200, () => {
                         $('.header').addClass('hide');
-                        $('html, body').scrollTop(0);
+                        $('html, body').addClass('no_flow');
+                        $('.view-section').addClass('so_flow');
                         $('.nav-wrapper').addClass('fixed_nav')
                     })
                 }
