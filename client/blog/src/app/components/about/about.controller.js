595b00665365bbe037214fb70bc536b085d5c194
relentless-coder <contact@ayushbahuguna.com>
2018-01-01 21:39:12 +0530

    remove the settimeout call from it

--- client/blog/src/app/components/about/about.controller.js
+++ client/blog/src/app/components/about/about.controller.js
@@ -4,11 +4,6 @@ function aboutController(userFactory, $localStorage, $rootScope){
       userFactory.getUserInfo().then((data)=>{
         ctrl.about = data.data.user.about;
       });
-      setTimeout(function () {
-        $('.home').animate({
-          opacity: 1
-        }, 'fast')
-      }, 1000);
   }
 }
 
