6691d03dbccf6f019cd195afd21d97e4a4a5d079
relentless-coder <contact@ayushbahuguna.com>
2018-01-01 08:02:40 +0530

    animate the opacity of the home wrapper

--- client/blog/src/app/components/about/about.controller.js
+++ client/blog/src/app/components/about/about.controller.js
@@ -1,9 +1,14 @@
 function aboutController(userFactory, $localStorage, $rootScope){
   const ctrl = this;
-  ctrl.$onInit = function(){  
+  ctrl.$onInit = function(){
       userFactory.getUserInfo().then((data)=>{
         ctrl.about = data.data.user.about;
       });
+      setTimeout(function () {
+        $('.home').animate({
+          opacity: 1
+        }, 'fast')
+      }, 1000);
   }
 }
 
