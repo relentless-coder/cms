9c7845eca555924a0b795e30ec35c4597813f83f
relentless-coder <contact@ayushbahuguna.com>
2018-01-01 08:05:09 +0530

    when the page reloads fix the nav on post

--- client/blog/src/app/components/post/post_single/post.single.controller.js
+++ client/blog/src/app/components/post/post_single/post.single.controller.js
@@ -1,11 +1,8 @@
 function postSingleController(postSingleFactory, $stateParams, $rootScope) {
   const ctrl = this;
   ctrl.$onInit = function () {
-    document.querySelector('.header').classList.add('zero_height')
+    $('.nav-wrapper').addClass('fixed_nav')
     document.querySelector('.posts').classList.add('hide');
-    setTimeout(function () {
-      document.querySelector('.nav-wrapper').classList.add('fixed_nav')
-    }, 500)
     postSingleFactory.getPost($stateParams.url).then((data) => {
       ctrl.post = data.data.post;
       ctrl.post.comments.forEach(el => el.replyVisible = false);
