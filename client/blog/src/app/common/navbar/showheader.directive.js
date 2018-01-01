3634d0a3b0242d35f6365b4a1f444fa4eac99686
relentless-coder <contact@ayushbahuguna.com>
2018-01-01 08:02:14 +0530

    scrollTop relevant to the nav-wrapper

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -5,15 +5,14 @@ function showHeader() {
             element.on('click', function () {
                 if (!$('.nav-wrapper.fixed_nav').length) {
                     $('html,body').animate({
-                        scrollTop: $('.view-section').offset().top
+                        scrollTop: $('.nav-wrapper').offset().top
                     }, 1100);
                     $('.profile-wrapper, .name-wrapper, .social-wrapper').animate({
                         opacity: 0
                     }, 1200, () => {
                         $('.header').addClass('hide');
-                        $('html, body').addClass('no_flow');
-                        $('.view-section').addClass('so_flow');
                         $('.nav-wrapper').addClass('fixed_nav')
+                        $('html').scrollTop(0);
                     })
                 }
             })
