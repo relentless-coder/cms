eef81e35b567d601d888b313d44c3d6402bacf0d
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 22:03:22 +0530

    hide and fix nav after the scroll

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -6,11 +6,12 @@ function showHeader($timeout) {
                 if (!$('.nav-wrapper.fixed_nav').length) {
                     $('html,body').animate({
                         scrollTop: $('.nav-wrapper').offset().top
-                    }, 1100);
-                    $('.header').addClass('hide');
-                    $('.nav-wrapper').addClass('fixed_nav')
-                    $('html').scrollTop(0);
-                    $('.view-section').addClass('opaque');
+                    }, 1100, ()=>{
+                      $('.header').addClass('hide');
+                      $('.nav-wrapper').addClass('fixed_nav')
+                      $('html').scrollTop(0);
+                      $('.view-section').addClass('opaque');
+                    });
                 } else {
                   $timeout(()=>{
                     $('.view-section').addClass('opaque');
