d69e41758ab03eb48204f5b4512ee073076c19a0
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 16:00:12 +0530

    remove console.log

--- client/dashboard/src/app/components/post/post_new/image.upload.provider.js
+++ client/dashboard/src/app/components/post/post_new/image.upload.provider.js
@@ -2,7 +2,6 @@ export const imageUpload = 'imageUpload'
 export const imageUploadFunction = function(){
   this.$get = function($http){
     function imageHandler(image){
-      console.log("You've reached the upload provider")
       $http.post('/post/images', image).then((data)=>{
         console.log(data);
       })
