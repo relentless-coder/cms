264a3f37454b06fbbfbb6640d7f91eab3f21bb62
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 21:45:59 +0530

    remove the opacity animation

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -3,19 +3,14 @@ function showHeader($timeout) {
         restrict: 'A',
         link: function (scope, element, attrs) {
             element.on('click', function () {
-
                 if (!$('.nav-wrapper.fixed_nav').length) {
                     $('html,body').animate({
                         scrollTop: $('.nav-wrapper').offset().top
                     }, 1100);
-                    $('.profile-wrapper, .name-wrapper, .social-wrapper').animate({
-                        opacity: 0
-                    }, 1200, () => {
-                        $('.header').addClass('hide');
-                        $('.nav-wrapper').addClass('fixed_nav')
-                        $('html').scrollTop(0);
-                        $('.view-section').addClass('opaque');
-                    })
+                    $('.header').addClass('hide');
+                    $('.nav-wrapper').addClass('fixed_nav')
+                    $('html').scrollTop(0);
+                    $('.view-section').addClass('opaque');
                 } else {
                   $timeout(()=>{
                     $('.view-section').addClass('opaque');
