00a7f83e76764317b6de24808bfa4225b679b8c9
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 22:03:41 +0530

    if the view section is not opaque, then make it

--- client/blog/src/app/components/about/about.controller.js
+++ client/blog/src/app/components/about/about.controller.js
@@ -1,6 +1,9 @@
 function aboutController(userFactory, $localStorage, $rootScope){
   const ctrl = this;
   ctrl.$onInit = function(){
+    if(!$('.view-section').hasClass('opaque')){
+        $('.view-section').addClass('opaque')
+    }
       userFactory.getUserInfo().then((data)=>{
         ctrl.about = data.data.user.about;
         $rootScope.meta.title = 'About | Ayush Bahuguna'
