5f1930a058f9281945abbe7860c4acf794da681f
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 22:19:24 +0530

    no need for the setTImeout

--- client/blog/src/app/common/navbar/showheader.directive.js
+++ client/blog/src/app/common/navbar/showheader.directive.js
@@ -6,16 +6,14 @@ function showHeader($timeout) {
                 if (!$('.nav-wrapper.fixed_nav').length) {
                     $('html,body').animate({
                         scrollTop: $('.nav-wrapper').offset().top
-                    }, 1100, ()=>{
+                    }, 'slow', ()=>{
                       $('.header').addClass('hide');
                       $('.nav-wrapper').addClass('fixed_nav')
                       $('html').scrollTop(0);
                       $('.view-section').addClass('opaque');
                     });
                 } else {
-                  $timeout(()=>{
-                    $('.view-section').addClass('opaque');
-                  }, 300)
+                  $('.view-section').addClass('opaque');
                 }
             })
         }
