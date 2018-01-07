b45341d00415e8ccff5b13cfcb6d51314f5b8750
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 16:06:34 +0530

    when not home set fixed_nav

--- client/blog/src/app/common/navbar/navbar.controller.js
+++ client/blog/src/app/common/navbar/navbar.controller.js
@@ -1,7 +1,9 @@
-function navbarController($localStorage){
+function navbarController($localStorage, $state){
     const ctrl = this;
     ctrl.navs = [];
     ctrl.$onInit = function(){
+        if($state.current.name === 'about' || $state.current.name === 'articles' || $state.current.name === 'articles.single' || $state.current.name === 'contact')
+          $('.nav-wrapper').addClass('fixed_nav')
         if($localStorage.user){
             $localStorage.user.navs.forEach(el => ctrl.navs.push({
               name: el,
