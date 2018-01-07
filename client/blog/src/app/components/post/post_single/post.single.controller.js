a4b0a85694189723ce79223ec0b5f3270a897205
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 16:08:17 +0530

    add keywords to rootScope as a string

--- client/blog/src/app/components/post/post_single/post.single.controller.js
+++ client/blog/src/app/components/post/post_single/post.single.controller.js
@@ -1,12 +1,12 @@
 function postSingleController(postSingleFactory, $stateParams, $rootScope) {
   const ctrl = this;
   ctrl.$onInit = function () {
-    $('.nav-wrapper').addClass('fixed_nav')
     document.querySelector('.posts').classList.add('hide');
     postSingleFactory.getPost($stateParams.url).then((data) => {
       ctrl.post = data.data.post;
       ctrl.post.comments.forEach(el => el.replyVisible = false);
       ctrl.content = ctrl.post.content;
+      ctrl.post.meta.keywords = ctrl.post.meta.keywords.join(',');
       $rootScope.meta = ctrl.post.meta;
     })
   }
