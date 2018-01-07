0ce6c0d5e58e6283e22ec635227986a950ef05eb
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 16:04:52 +0530

    on reload check for opaque on view-section

--- client/blog/src/app/components/post/posts/posts.controller.js
+++ client/blog/src/app/components/post/posts/posts.controller.js
@@ -1,7 +1,9 @@
-function postsController($localStorage, postsFactory, $rootScope) {
+function postsController($localStorage, postsFactory, $rootScope, $state) {
   const ctrl = this;
   ctrl.$onInit = function() {
-
+    if(!$('.view-section').hasClass('opaque')){
+      $('.view-section').addClass('opaque');
+    }
     postsFactory.getPosts().then((data) => {
       $rootScope.posts = data.data.published;
     })
