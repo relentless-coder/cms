987bff4a60c6c0861054a557e5b0bfa25891ef26
relentless-coder <contact@ayushbahuguna.com>
2018-01-07 16:09:17 +0530

    on reload check for the opaque view section

--- client/blog/src/app/components/contact/contact.controller.js
+++ client/blog/src/app/components/contact/contact.controller.js
@@ -1,5 +1,8 @@
 function contactController(contactFactory){
   const ctrl = this;
+  if(!$('.view-section').hasClass('opaque')){
+    $('.view-section').addClass('opaque');
+  }
   ctrl.contact = function(){
     contactFactory.contact(ctrl.user).then((data)=>{
       console.log(data);
